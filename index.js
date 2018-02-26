f6879ab5e4870aa2945d65b252b9bcddcb5ef6f0
Thodoris Bais <thodoris.bais@gmail.com>
2018-02-26 02:09:18 +0100

    Better design

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
 'use strict';
 
 // List extracted from the Hard Keywords section of http://kotlinlang.org/docs/reference/keyword-reference.html
-var reservedKeywords = {
-    as: true,
-    break: true,
-    class: true,
-    continue: true,
-    do: true,
-    else: true,
-    false: true,
-    for: true,
-    fun: true,
-    if: true,
-    in: true,
-    interface: true,
-    is: true,
-    null: true,
-    object: true,
-    package: true,
-    return: true,
-    super: true,
-    this: true,
-    throw: true,
-    true: true,
-    try: true,
-    typealias: true,
-    val: true,
-    var: true,
-    when: true,
-    while: true
-};
+const reservedKeywords = [
+    'AS', 'BREAK', 'CLASS', 'CONTINUE', 'DO', 'ELSE', 'FALSE', 'FOR', 'FUN', 'IF', 'IN', 'INTERFACE', 'IS', 'NULL',
+    'OBJECT', 'PACKAGE', 'RETURN', 'SUPER', 'THIS', 'THROW', 'TRUE', 'TRY', 'TYPEALIAS', 'VAL', 'VAR', 'WHEN', 'WHILE'
+];
 
 module.exports = function(str) {
-    return reservedKeywords.hasOwnProperty(str);
+    return reservedKeywords.includes(str.toUpperCase());
 };
